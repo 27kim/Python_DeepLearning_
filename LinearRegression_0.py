099e8f373e17ad3650336516cbc44a78ad961584
27k <doohyeonkim@gmail.com>
2018-12-18 11:47:01 +0900

    minimizing_cost

--- LinearRegression_0.py
+++ LinearRegression_0.py
@@ -1,4 +1,4 @@
-#tensorflow import
+# tensorflow import
 import tensorflow as tf
 
 # 데이터
@@ -9,15 +9,18 @@ y_data = [1., 2., 3.]
 w = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 b = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 
-hypothesis = w * x_data + b
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
+
+hypothesis = w * X + b
 
 # cost 구하는 식 생성
-cost = tf.reduce_mean(tf.square(hypothesis - y_data))
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
 
 # Gradient Decent 가져오기 (2차 함수이므로 결국에는 기울기가 0인 곳이 최소값일 것)
 optimizer = tf.train.GradientDescentOptimizer(0.1)
 
-#optimizer 가 최소값 구할 수 있도록
+# optimizer 가 최소값 구할 수 있도록
 train = optimizer.minimize(cost)
 
 # tensorflow 사용하기 위한 변수 초기화
@@ -30,6 +33,8 @@ sess = tf.Session()
 sess.run(init)
 
 for step in range(2001):
-    sess.run(train)
+    sess.run(train, feed_dict={X: x_data, Y: y_data})
     if step % 20 == 0:
-        print(step, sess.run(cost), sess.run(w), sess.run(b))
+        print(step, sess.run(cost, feed_dict={X: x_data, Y: y_data}), sess.run(w), sess.run(b))
+
+print("결과 ? %s" %sess.run(hypothesis, feed_dict={X: 5}))
