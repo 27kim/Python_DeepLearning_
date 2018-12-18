beb7d5e9b6c9973ce214673d6fb3fce2ade6d389
27k <doohyeonkim@gmail.com>
2018-12-18 11:10:48 +0900

    주석 추가

--- LinearRegression_0.py
+++ LinearRegression_0.py
@@ -14,7 +14,7 @@ hypothesis = w * x_data + b
 # cost 구하는 식 생성
 cost = tf.reduce_mean(tf.square(hypothesis - y_data))
 
-# Gradient Decent 가져오기
+# Gradient Decent 가져오기 (2차 함수이므로 결국에는 기울기가 0인 곳이 최소값일 것)
 optimizer = tf.train.GradientDescentOptimizer(0.1)
 
 #optimizer 가 최소값 구할 수 있도록
